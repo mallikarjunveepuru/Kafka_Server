e0b4fdbe4b9dc7d30271803d633feeddc9e03b5e
mallikarjunveepuru <malliveepuru@gmail.com>
2016-10-25 15:52:05 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "started"
-apt-get update
-apt-get remove scala-library scala
+apt-get update -y
+apt-get remove scala-library scala -y
 apt-get install wget -y
 rm -rf scala-2.10.4.deb
 wget www.scala-lang.org/files/archive/scala-2.10.4.deb
 dpkg -i scala-2.10.4.deb
 apt-get -f install -y
-apt-get install scala
+apt-get install scala -y
 rm -rf /etc/apt/sources.list.d/sbt.list
 echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-apt-get update
+apt-get update -y
 apt-get install sbt -y
 rm -rf kafka_2.10-0.9.0.1*
 wget http://www-us.apache.org/dist/kafka/0.9.0.1/kafka_2.10-0.9.0.1.tgz
