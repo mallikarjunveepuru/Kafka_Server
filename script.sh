6ee8d9fdeba515225572e39e0c5d38f15de788b3
mallikarjunveepuru <malliveepuru@gmail.com>
2016-10-28 12:14:45 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~
 echo "started"
 apt-get update -y
 apt-get remove scala-library scala -y
@@ -19,27 +20,28 @@ curl -O https://storage.googleapis.com/gggopaddle1/kafka_2.10-0.9.0.1.zip
 sleep 15
 unzip kafka_2.10-0.9.0.1.zip
 ip="$(curl icanhazip.com)"
-sed -i "s/localhost/${ip}/g" kafka_2.10-0.9.0.1/config/producer.properties
-sed -i "s/localhost/${ip}/g" kafka_2.10-0.9.0.1/config/server.properties
-sed -i "s/127.0.0.1/${ip}/g" kafka_2.10-0.9.0.1/config/consumer.properties
-sed -i "s/2181/30000/g" kafka_2.10-0.9.0.1/config/server.properties
-sed -i "s/2181/30000/g" kafka_2.10-0.9.0.1/config/consumer.properties 
-sed -i "s/2181/30000/g" kafka_2.10-0.9.0.1/config/zookeeper.properties
-sed -i "s/9092/30001/g" kafka_2.10-0.9.0.1/config/server.properties
-sed -i "s/9092/30001/g" kafka_2.10-0.9.0.1/config/connect-standalone.properties 
-sed -i "s/9092/30001/g" kafka_2.10-0.9.0.1/config/connect-distributed.properties 
-sed -i "s/localhost/${ip}/g" kafka_2.10-0.9.0.1/config/connect-standalone.properties 
-sed -i "s/localhost/${ip}/g" kafka_2.10-0.9.0.1/config/connect-distributed.properties 
-sed -i "s/9092/30001/g" kafka_2.10-0.9.0.1/config/producer.properties
+sed -i "s/localhost/${ip}/g" ~/kafka_2.10-0.9.0.1/config/producer.properties
+sed -i "s/localhost/${ip}/g" ~/kafka_2.10-0.9.0.1/config/server.properties
+sed -i "s/127.0.0.1/${ip}/g" ~/kafka_2.10-0.9.0.1/config/consumer.properties
+sed -i "s/2181/30000/g" ~/kafka_2.10-0.9.0.1/config/server.properties
+sed -i "s/2181/30000/g" ~/kafka_2.10-0.9.0.1/config/consumer.properties 
+sed -i "s/2181/30000/g" ~/kafka_2.10-0.9.0.1/config/zookeeper.properties
+sed -i "s/9092/30001/g" ~/kafka_2.10-0.9.0.1/config/server.properties
+sed -i "s/9092/30001/g" ~/kafka_2.10-0.9.0.1/config/connect-standalone.properties 
+sed -i "s/9092/30001/g" ~/kafka_2.10-0.9.0.1/config/connect-distributed.properties 
+sed -i "s/localhost/${ip}/g" ~/kafka_2.10-0.9.0.1/config/connect-standalone.properties 
+sed -i "s/localhost/${ip}/g" ~/kafka_2.10-0.9.0.1/config/connect-distributed.properties 
+sed -i "s/9092/30001/g" ~/kafka_2.10-0.9.0.1/config/producer.properties
 #kafka_2.10-0.9.0.1/bin/zookeeper-server-start.sh kafka_2.10-0.9.0.1/config/zookeeper.properties&
-kafka_2.10-0.9.0.1/bin/kafka-server-start.sh kafka_2.10-0.9.0.1/config/server.properties&
+~/kafka_2.10-0.9.0.1/bin/kafka-server-start.sh kafka_2.10-0.9.0.1/config/server.properties&
 #kafka_2.10-0.9.0.1/bin/kafka-console-producer.sh --topic test --broker-list localhost:9092
 curl -O http://d3kbcqa49mib13.cloudfront.net/spark-1.6.0-bin-hadoop2.6.tgz
-tar xvf spark-1.6.0-bin-hadoop2.6.tgz
+tar xvf ~/spark-1.6.0-bin-hadoop2.6.tgz
 ./spark-1.6.0-bin-hadoop2.6/sbin/start-master.sh -h 0.0.0.0
+apt-get install git -y
 git clone https://github.com/mallikarjunveepuru/sample-KafkaSparkCassandra.git
-cd sample-KafkaSparkCassandra
+cd ~/sample-KafkaSparkCassandra
 sbt assembly
 cd ..
-./spark-1.6.0-bin-hadoop2.6/sbin/start-slave.sh spark://0.0.0.0:7007
+~/spark-1.6.0-bin-hadoop2.6/sbin/start-slave.sh spark://0.0.0.0:7007
 ~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --properties-file cassandra-count.conf --class KafkaSparkCassandra target/scala-2.10/cassandra-kafka-streaming-assembly-1.0.jar
