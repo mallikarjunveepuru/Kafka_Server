f68e5f9d68c601c719b1ce197a970d2b549e901e
mallikarjunveepuru <malliveepuru@gmail.com>
2016-10-28 20:25:37 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -32,16 +32,19 @@ sed -i "s/9092/30001/g" ~/kafka_2.10-0.9.0.1/config/connect-distributed.properti
 sed -i "s/localhost/${ip}/g" ~/kafka_2.10-0.9.0.1/config/connect-standalone.properties 
 sed -i "s/localhost/${ip}/g" ~/kafka_2.10-0.9.0.1/config/connect-distributed.properties 
 sed -i "s/9092/30001/g" ~/kafka_2.10-0.9.0.1/config/producer.properties
+sed -i "s/#host.name=${ip}/host.name=${ip}/g" ~/kafka_2.10-0.9.0.1/config/server.properties
+sudo iptables -t nat -A POSTROUTING ! -d 10.0.0.0/8 -o ens4v1 -j MASQUERADE
+sed -i "s/#advertised.host.name=<hostname routable by clients>/advertised.host.name=${ip}/g" ~/kafka_2.10-0.9.0.1/config/server.properties
+sed -i "s/#advertised.port=<port accessible by clients>/advertised.port=9092/g" ~/kafka_2.10-0.9.0.1/config/server.properties
 #kafka_2.10-0.9.0.1/bin/zookeeper-server-start.sh kafka_2.10-0.9.0.1/config/zookeeper.properties&
 ~/kafka_2.10-0.9.0.1/bin/kafka-server-start.sh kafka_2.10-0.9.0.1/config/server.properties&
 #kafka_2.10-0.9.0.1/bin/kafka-console-producer.sh --topic test --broker-list localhost:9092
 curl -O http://d3kbcqa49mib13.cloudfront.net/spark-1.6.0-bin-hadoop2.6.tgz
-tar xvf ~/spark-1.6.0-bin-hadoop2.6.tgz
-~/spark-1.6.0-bin-hadoop2.6/sbin/start-master.sh -h 0.0.0.0
-sleep 15
-apt-get install git -y
+tar xvf spark-1.6.0-bin-hadoop2.6.tgz
 git clone https://github.com/mallikarjunveepuru/sample-KafkaSparkCassandra.git
 cd ~/sample-KafkaSparkCassandra
 sbt assembly
-~/spark-1.6.0-bin-hadoop2.6/sbin/start-slave.sh spark://0.0.0.0:7007
+~/spark-1.6.0-bin-hadoop2.6/sbin/start-master.sh -h ${ip} -p 7077
+export SPARK_LOCAL_IP=${ip}
+~/spark-1.6.0-bin-hadoop2.6/sbin/start-slave.sh spark://${ip}:7077
 ~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --properties-file cassandra-count.conf --class KafkaSparkCassandra target/scala-2.10/cassandra-kafka-streaming-assembly-1.0.jar
