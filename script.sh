d4994d5e7e9e78507906264bc3b734f8f2ee01a2
mallikarjunveepuru <malliveepuru@gmail.com>
2016-11-14 18:04:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,15 +33,17 @@ sed -i "s/#advertised.port=<port accessible by clients>/advertised.port=9092/g" 
 #kafka_2.10-0.9.0.1/bin/zookeeper-server-start.sh kafka_2.10-0.9.0.1/config/zookeeper.properties&
 ~/kafka_2.10-0.9.0.1/bin/kafka-server-start.sh kafka_2.10-0.9.0.1/config/server.properties&
 #kafka_2.10-0.9.0.1/bin/kafka-console-producer.sh --topic test --broker-list localhost:9092
-curl -O http://d3kbcqa49mib13.cloudfront.net/spark-1.6.0-bin-hadoop2.6.tgz
-sleep 30
-tar xvf spark-1.6.0-bin-hadoop2.6.tgz
-sleep 10
-git clone https://github.com/mallikarjunveepuru/sample-KafkaSparkCassandra.git
-cd ~/sample-KafkaSparkCassandra
-sbt assembly
-~/spark-1.6.0-bin-hadoop2.6/sbin/start-master.sh -h 0.0.0.0 -p 7077
-export SPARK_LOCAL_IP=0.0.0.0
-#ip="$(curl icanhazip.com)"
-~/spark-1.6.0-bin-hadoop2.6/sbin/start-slave.sh spark://localhost:7077 -m 128M
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --properties-file cassandra-count.conf --class KafkaSparkCassandra target/scala-2.10/cassandra-kafka-streaming-assembly-1.0.jar
+
+
+
+#curl -O http://d3kbcqa49mib13.cloudfront.net/spark-1.6.0-bin-hadoop2.6.tgz
+#tar xvf spark-1.6.0-bin-hadoop2.6.tgz
+#sleep 10
+#git clone https://github.com/mallikarjunveepuru/sample-KafkaSparkCassandra.git
+#cd ~/sample-KafkaSparkCassandra
+#sbt assembly
+#~/spark-1.6.0-bin-hadoop2.6/sbin/start-master.sh -h 0.0.0.0 -p 7077
+#export SPARK_LOCAL_IP=0.0.0.0
+##########################ip="$(curl icanhazip.com)"
+#~/spark-1.6.0-bin-hadoop2.6/sbin/start-slave.sh spark://localhost:7077 -m 128M
+#~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --properties-file cassandra-count.conf --class KafkaSparkCassandra target/scala-2.10/cassandra-kafka-streaming-assembly-1.0.jar
